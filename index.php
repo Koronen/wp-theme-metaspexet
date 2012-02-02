927a3c8767627e946b08cf057741908a90c1d5f7
Kristoffer Emanuelsson <kristoffer.emanuelsson@gmail.com>
2012-02-02 18:24:52 +0100

    Right Sidebar and start of Posts

--- index.php
+++ index.php
@@ -12,45 +12,39 @@ get_header(); ?>
 
         <?php while (have_posts()) : the_post(); ?>
 
-                <div <?php post_class() ?> id="post-<?php the_ID(); ?>">
-                        <div class="post-date">
+                <article <?php post_class() ?> id="post-<?php the_ID(); ?>">
+<!--                        <div class="post-date">
                                 <a href="<?php the_permalink() ?>" rel="bookmark" title="<?php _e( 'Permanent Link to' , 'metaspexet' ) ?> <?php the_title_attribute(); ?>">
                                 <span class="post-month"><?php the_time('M'); ?></span>
                                 <span class="post-day"><?php the_time('j'); ?></span>
                                 <span class="post-year"><?php the_time('Y'); ?></span>
                                 </a>
-                        </div>
-                        <?php edit_post_link( __( 'Edit', 'metaspexet' ), '<div class="edit-post-link">', '</div>') ?>	
-                        <?php if ( has_post_thumbnail() ) { 
+                        </div>-->
+                        <?php // edit_post_link( __( 'Edit', 'metaspexet' ), '<div class="edit-post-link">', '</div>') ?>
+                        <?php if ( has_post_thumbnail() ) {
                                 echo "<div class='post-thumbnail'>";
                                         echo "<a href=\"" . get_permalink() . "\">";
                                         the_post_thumbnail();
                                         echo "</a>";
                                 echo "</div>";
                         } ?>
-                        <h2 class="page_title">
+                        <h1 class="page_title">
                                 <a href="<?php the_permalink() ?>" rel="bookmark" title="<?php _e( 'Permanent Link to' , 'metaspexet' ) ?> <?php the_title_attribute(); ?>"><?php the_title(); ?></a>
-                        </h2>
-                        <small><?php _e( 'by' , 'metaspexet' ); ?> <?php the_author() ?></small>
+                        </h1>
+<!--                        <small><?php _e( 'by' , 'metaspexet' ); ?> <?php the_author() ?></small>-->
                         <div class="entry">
                                 <?php the_content( __( 'Read the rest of this entry' , 'metaspexet' ) . ' &raquo;'); ?>
                                 <?php wp_link_pages(array('before' => '<p class="clear">' . __( 'Pages:' , 'metaspexet' ) . ' ', 'after' => '</p>', 'next_or_number' => 'number')); ?>
 
-                                <p class="postmetadata clear">
-                                        <?php _e( 'Categories:' , 'metaspexet' ) ?> <?php the_category(', ') ?> | 
+<!--                                <p class="postmetadata clear">
+                                        <?php _e( 'Categories:' , 'metaspexet' ) ?> <?php the_category(', ') ?> |
                                         <?php comments_popup_link( __( 'No Comments' , 'metaspexet' ) . ' &#187;', '1 ' . __( 'Comment' , 'metaspexet' ) . ' &#187;', '% ' . __( 'Comments' , 'metaspexet' ) . ' &#187;'); ?>
-                                </p>
-                        
-                        </div>
-                </div>
-                <hr class="divider" />
-    
-                <?php endwhile; ?>
+                                </p>-->
 
-        <div class="navigation">
-                <div class="alignleft"><?php next_posts_link('&laquo;' . __( 'Older Entries' , 'metaspexet' ) ) ?></div>
-                <div class="alignright"><?php previous_posts_link( __( 'Newer Entries' , 'metaspexet' ) . ' &raquo;') ?></div>
-        </div>
+                        </div>
+                </article>
+
+                <?php endwhile; ?>
 
 <?php else : ?>
 
--- sidebar-right.php
+++ sidebar-right.php
@@ -7,7 +7,27 @@
  */
 ?>
 <ul id="widget-area-right" class="widget-area">
+    <li id="biljetter" class="widget-container widget_text">
+        <div class="textwidget">
+            <nav>
+                <ul id="biljetter_ul">
+                    <li>
+                        <a id="kop_biljetter" href="/kop_biljetter/" title="Köp Biljetter"></a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </li>
+    <li id="tv_books" class="widget-container widget_text">
+        <div class="textwidget">
+            <img src="<?php bloginfo('template_directory'); ?>/img/tv_bocker.png" alt="Tv och Böcker" title="Tv och Böcker" />
+        </div>
+    </li>
+    <li id="poster" class="widget-container widget_text">
+        <div class="textwidget">
+            <img src="<?php bloginfo('template_directory'); ?>/img/poster.png" alt="Se så det gnistrar" title="Se så det gnistrar" />
+        </div>
+    </li>
     <?php if ( !dynamic_sidebar('right-widget-area') ) : // begin right widget area ?>
-        This is the right sidebar... Should be widgets here!
     <?php endif; // end right widget area ?>
 </ul>
