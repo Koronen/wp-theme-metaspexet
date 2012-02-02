9d6f8fb1d4a3d815e0c747e47c206a692f29b240
Kristoffer Emanuelsson <kristoffer.emanuelsson@gmail.com>
2012-02-02 17:47:46 +0100

    Senaste Nytt

--- sidebar-left.php
+++ sidebar-left.php
@@ -27,12 +27,24 @@
             </nav>
         </div>
     </li>
+    <li id="latest_news" class="widget-container widget_text">
+        <div class="textwidget">
+            <ul id="latest_news_list">
+                <?php
+                global $post;
+                $args = array( 'numberposts' => 5 );
+                $myposts = get_posts( $args );
+                foreach( $myposts as $post ) :	setup_postdata($post); ?>
+                    <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+                <?php endforeach; ?>
+            </ul>
+        </div>
     <?php if ( !dynamic_sidebar('left-widget-area') ) : // begin left widget area ?>
-    <li id="archives" class="widget-container">
+<!--    <li id="archives" class="widget-container">
             <h3 class="widget-title"><?php _e( 'Archives', 'twentyten' ); ?></h3>
             <ul>
                     <?php wp_get_archives( 'type=monthly' ); ?>
             </ul>
-    </li>
+    </li>-->
     <?php endif; // end left widget area ?>
 </ul>
