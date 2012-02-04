6a8a9871b56681f559a7b0e2b788d8ed71cbbc03
Victor Koronen <koronen@kth.se>
2012-02-04 16:28:52 +0100

    Remove unused archive widget

--- sidebar-left.php
+++ sidebar-left.php
@@ -38,12 +38,5 @@
                 <?php endforeach; ?>
             </ul>
         </div>
-    <?php if ( !dynamic_sidebar('left-widget-area') ) : // begin left widget area ?>
-<!--    <li id="archives" class="widget-container">
-            <h3 class="widget-title"><?php _e( 'Archives', 'twentyten' ); ?></h3>
-            <ul>
-                    <?php wp_get_archives( 'type=monthly' ); ?>
-            </ul>
-    </li>-->
-    <?php endif; // end left widget area ?>
+    </li>
 </ul>
