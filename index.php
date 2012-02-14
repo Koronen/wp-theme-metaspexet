228ab739f47941f5cac6df2dff73fdcd3046e8ad
Kristoffer Emanuelsson <kristoffer.emanuelsson@gmail.com>
2012-02-14 17:23:08 +0100

    Posts top and bottom

--- index.php
+++ index.php
@@ -12,6 +12,8 @@ get_header(); ?>
 
         <?php while (have_posts()) : the_post(); ?>
 
+    <div class="post_container">
+                <div class="post_top"></div>
                 <article <?php post_class() ?> id="post-<?php the_ID(); ?>">
 <!--                        <div class="post-date">
                                 <a href="<?php the_permalink() ?>" rel="bookmark" title="<?php _e( 'Permanent Link to' , 'metaspexet' ) ?> <?php the_title_attribute(); ?>">
@@ -45,6 +47,8 @@ get_header(); ?>
 
                         </div>
                 </article>
+                <div class="post_bottom"></div>
+    </div>
 
                 <?php endwhile; ?>
 
